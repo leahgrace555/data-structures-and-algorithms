75802df842c40023d5705f88915535688a7d01a0
Leah <leah.grace555@gmail.com>
2020-09-15 21:12:42 -0400

    initialized gradle

---
+++ challenges/ArrayShiftTest.java
@@ -0,0 +1,11 @@
+package gradle.practice;
+
+import org.junit.Test;
+import static org.junit.Assert;
+
+public class ArrayShiftTest {
+    @Test public void testArrayShift() {
+        Library classUnderTest = new Library();
+        System.out.println("what does this do");
+    }
+}
