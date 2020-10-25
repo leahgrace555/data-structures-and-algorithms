2b9ffc8d984d3715fbd0f4a435422e909224375a
leahgrace555 <leah.grace555@gmail.com>
2020-10-25 11:19:58 -0400

    added reverse array function to code challenge 1

--- code-challenges/challenges/arrayReverse/array-reverse.js
+++ code-challenges/challenges/arrayReverse/array-reverse.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function reverseArray (arr) {
+  let forwardArr = arr;
+  let reversedArr = [];
+  for(let i = forwardArr.length -1; i>=0; i--) {
+    reversedArr.push(arr[i]);
+  }
+  console.log(reversedArr);
+}
+
+let testarr = [1,2,3,4]
+
+reverseArray(testarr)
+
