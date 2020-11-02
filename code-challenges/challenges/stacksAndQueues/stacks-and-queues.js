57618d67b8813285234bb3a6d4d39919313379d0
leahgrace555 <leah.grace555@gmail.com>
2020-11-02 10:03:26 -0500

    completed multi-bracket validation function, need to fix modularization issues;

--- code-challenges/challenges/stacksAndQueues/stacks-and-queues.js
+++ code-challenges/challenges/stacksAndQueues/stacks-and-queues.js
@@ -142,6 +142,38 @@ class PseudoQueue {
   }
 }
 
+
+function bracketValidation(string) {
+  // validates whether the brackets in a string are balanced
+   
+    let bracketStack = new Stack();
+console.log(string.length)
+    for (let i = 0; i < string.length; i++) {
+      if(string[i] === '[' || string[i] === '(' || string[i] === '{') {
+        bracketStack.push(string[i])
+      } 
+            if(!bracketStack.top) {
+              return false;
+            }
+      else if(string[i] === ']' || string[i] === ')' || string[i] === '}') {
+        if((string[i] === ']' && bracketStack.top.value === '[' )|| (string[i] === '}' && bracketStack.top.value === '{' )|| (string[i] === ')' && bracketStack.top.value === '(')) {
+          bracketStack.pop();
+        }
+        else {
+          return false;
+       } 
+      }
+    }
+    if(bracketStack.top) {
+      return false;
+    }
+    return true;
+}
+
+console.log('BRACKET VALIDATION:')
+console.log(bracketValidation('{)'));
+
+
 let testQueue = new Queue();
 //console.log(testQueue.isEmpty());
 
@@ -151,7 +183,7 @@ testPseudoQueue.enqueue(42);
 console.log( 'this is the pseudoqueue')
 console.log(testPseudoQueue);
 
-testPseudoQueue.dequeue();
+//testPseudoQueue.dequeue();
 
 
 // testQueue.enqueue(565)
@@ -163,3 +195,4 @@ testPseudoQueue.dequeue();
 // console.log(testQueue.isEmpty());
 
 module.exports = Stack;
+module.exports = Queue;
