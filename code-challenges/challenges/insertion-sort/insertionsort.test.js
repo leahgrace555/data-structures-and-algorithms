5c70bb1d8cbb6b5ac25973ca8a971d0c37870b28
leahgrace555 <leah.grace555@gmail.com>
2020-11-21 14:36:01 -0500

    insertion sort complete

--- code-challenges/challenges/insertion-sort/insertionsort.test.js
+++ code-challenges/challenges/insertion-sort/insertionsort.test.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const insertionSort = require('./insertionsort.js')
+const insertionsort = require('./insertionsort.js');
+
+
 ;
 describe('Testing challenge 26', ()=> {
   it('Should sort the array in ascending order', ()=> {
     let arr = [8,4,23,42,16,15];
     let expectedArr = [4,8,15,16,23,42];
 
-    expect(arr.insertionSort().toEqual(expectedArr));
+    expect(insertionsort(arr)).toEqual(expectedArr);
   })
 
   it('Should be able to sort an array with few unique numbers', ()=> {
     let testArr = [5,12,7,5,5,7];
     let expectedArr = [5,5,5,7,7,12];
 
-    expect(arr.insertionSort().toEqual(expectedArr));
+    expect(insertionsort(testArr)).toEqual(expectedArr);
   })
 })
