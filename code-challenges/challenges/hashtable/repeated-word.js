2c60e535e457993b0ca01543af61054fb066ffc1
leahgrace555 <leah.grace555@gmail.com>
2020-11-24 15:57:01 -0500

    testing

--- code-challenges/challenges/hashtable/repeated-word.js
+++ code-challenges/challenges/hashtable/repeated-word.js
@@ -4,3 +4,19 @@ const hashTable = require('./hashtable.js');
 const HashTable = hashTable.hashTable;
 const Node = hashTable.node;
 const LinkedList = hashTable.linkedList;
+
+function findFirstRepeat(str) {
+
+  let words = str.split(' ');
+  let storedWord = new HashTable();
+
+  for(let i = 0; i< words.length; i++) {
+    if(storedWord.contains(words[i])) {
+      return words[i];
+    } else
+  }
+}
+
+let str = 'this is a string and stuff';
+let splitstr = str.split(' ');
+console.log(splitstr);
---
+++ code-challenges/challenges/treeintersection/tree-intersection.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const Tree = require('../binaryTree/binary-tree.js');
+
+describe('testing challenge 32', ()=> {
+  it('should return an empty array if there are no matches' ,()=> {
+    const treeOne = new Tree.tree();
+    
+  })
+})
