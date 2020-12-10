c4965a9a3678bc363ae136163801161dbd93c6c8
leahgrace555 <leah.grace555@gmail.com>
2020-12-09 19:48:50 -0500

    testing get edges

--- code-challenges/challenges/graphs/get-edge.test.js
+++ code-challenges/challenges/graphs/get-edge.test.js
@@ -1,25 +1,58 @@
 const { Vertex, Edge, Graph } = require('./graph.js');
+const getEdge = require('./get-edge.js')
 
-describe('testing challenge 37', ()=> {
-  it('should be able to get a vertex neighbors', ()=> {
+// describe('testing challenge 37', ()=> {
+//   it('should be able to get a vertex neighbors', ()=> {
 
-    const graph = new Graph();
+//     const graph = new Graph();
 
+//     const A = graph.addVertex('Pandora');
+//     const B = graph.addVertex('Metroville');
+//     const C = graph.addVertex('Narnia');
+//     const D = graph.addVertex('Naboo');
+
+//     graph.addEdge(A, B, weight=82);
+//     graph.addEdge(B,C, weight=37);
+//     graph.addEdge(C,D,weight=250);
+//     graph.addEdge(B,D,weight = 26);
+
+    
+
+//     // graph.getNeighbors(A).forEach(thing => {
+//     //   console.log(thing.vertex.value)
+//     // })
+
+//     //console.log(graph.depthFirst(A))
+
+//     // graph.depthFirst(A).forEach( thing => {
+//     //   console.log(thing.value)
+//     // })
+
+
+//     getEdge(graph,[1,2])
+
+
+//       //console.log(graph.adjacencyList.Map)
+//   })
+// })
+
+const graph = new Graph();
     const A = graph.addVertex('Pandora');
     const B = graph.addVertex('Metroville');
     const C = graph.addVertex('Narnia');
     const D = graph.addVertex('Naboo');
-
+    const E = graph.addVertex('Corresant');
     graph.addEdge(A, B, weight=82);
     graph.addEdge(B,C, weight=37);
-    graph.addEdge(C,D,weight=250);
-    graph.addEdge(B,D,weight = 26);
-
-    console.log(graph.getNeighbors(A));
-
-    graph.getNeighbors(A).forEach(thing => {
-      console.log(thing.vertex.value)
+    graph.addEdge(C,D, weight=250);
+    graph.addEdge(B,D, weight = 26);
+    graph.addEdge(A,E, weight = 50000);
+    const cities = ['Pandora', 'Metroville']
+describe('getEdge', () => {
+    it('happy path', () => {
+        expect(getEdge(graph, cities)).toEqual({totalCost: 82, true: true});
+        expect(getEdge(graph, ['Pandora', 'Metroville', 'Naboo'])).toEqual({totalCost: 108, true: true})
+        expect(getEdge(graph, ['Pandora', 'Naboo'])).toEqual({totalCost: 0, false: false})
+        expect(getEdge(graph, ['Pandora', 'Metroville', 'Corresant'])).toEqual({totalCost: 0, false: false})
     })
-
-  })
 })
